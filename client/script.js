c616f5bdd3fc1bb1126f873f65497071ee917fff
Paulo <paulozfilho2@gmail.com>
2025-06-10 18:41:20 -0300

    Correção Script.js

--- client/script.js
+++ client/script.js
@@ -35,7 +35,7 @@ form.addEventListener('submit', async (e) => {
   const password = passwordInput.value;
 
   try {
-    const res = await fetch('https://autentificador-api.onrender.com/register', {
+    const res = await fetch('https://autentificador-api.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
